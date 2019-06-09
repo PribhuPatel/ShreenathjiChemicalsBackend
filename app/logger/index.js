74231f6899c0e834a3b59927342a5aaa608a4d64
Priyank Bhuva <160010116006@adit.ac.in>
2019-06-10 01:23:42 +0530

    mongo host changed

--- app/logger/index.js
+++ app/logger/index.js
@@ -1,41 +1,41 @@
-const winston = require('winston');
-// const logger = new winston.Logger({
+// const winston = require('winston');
+// // const logger = new winston.Logger({
+// //   level: 'error',
+// //   transports: [
+// //     new (winston.transports.Console)(),
+// //     new (winston.transports.File)({ filename: __dirname+'/error-log.log' })
+// //   ]
+// // });
+//
+// // const logger = winston.createLogger({
+// //   level: 'error',
+// //   transports: [
+// //     new (winston.transports.Console)(),
+// //     new (winston.transports.File)({ filename: __dirname+'/error-log.log' })
+// //   ],
+// //   exitOnError: false, // do not exit on handled exceptions
+// // });
+//
+// const errQueryOptions = {
+//   from: new Date - 24 * 60 * 60 * 1000,
+//   until: new Date,
+//   limit: 10,
+//   start: 0,
 //   level: 'error',
-//   transports: [
-//     new (winston.transports.Console)(),
-//     new (winston.transports.File)({ filename: __dirname+'/error-log.log' })
-//   ]
-// });
-
-const logger = winston.createLogger({
-  level: 'error',
-  transports: [
-    new (winston.transports.Console)(),
-    new (winston.transports.File)({ filename: __dirname+'/error-log.log' })
-  ],
-  exitOnError: false, // do not exit on handled exceptions
-});
-
-const errQueryOptions = {
-  from: new Date - 24 * 60 * 60 * 1000,
-  until: new Date,
-  limit: 10,
-  start: 0,
-  level: 'error',
-  order: 'desc',
-};
-
-module.exports = {
-  /* logging the errors */
-  logTheError: ( error ) =>{
-    logger.log('error', error);
-  },
-  /* querying the errors */
-  queryErrors: ( from = new Date, until = new Date) =>{
-    return new Promise(( resolve, reject ) =>{
-      logger.query({...errQueryOptions,  from, until }, function (err, results) {
-        ( err ? reject(err) : resolve(results) );
-      });
-    })
-  }
-};
+//   order: 'desc',
+// };
+//
+// module.exports = {
+//   /* logging the errors */
+//   logTheError: ( error ) =>{
+//     // logger.log('error', error);
+//   },
+//   /* querying the errors */
+//   queryErrors: ( from = new Date, until = new Date) =>{
+//     return new Promise(( resolve, reject ) =>{
+//       // logger.query({...errQueryOptions,  from, until }, function (err, results) {
+//         ( err ? reject(err) : resolve(results) );
+//       });
+//     })
+//   }
+// };
